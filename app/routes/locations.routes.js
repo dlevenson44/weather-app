2b7c300bd3bab263eb9f08287616596d079f7e2a
dlevenson44 <daniellevenson4@gmail.com>
2025-10-20 23:28:43 -0400

    setup router for favorites

---
+++ app/routes/locations.routes.js
@@ -0,0 +1,14 @@
+import express from "express"
+import * as locations from "../controllers/location.controller"
+
+export default (app) => {
+  const router = express.Router()
+  router.get('/', locations.findAll)
+  router.get('/:id', locations.findOne)
+  router.post('/', locations.create)
+  router.put('/:id', locations.update)
+  router.delete('/', locations.deleteAll)
+  router.delete('/:id', locations.deleteOne)
+
+  app.use('/api/locations', router)
+}
